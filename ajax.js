6899f3a692d36aec41f47866b6abb89822cb6e4c
niemistaru <taru.nieminen@student.laurea.fi>
2022-11-25 17:52:05 +0200

    Genrehommat

--- ajax.js
+++ ajax.js
@@ -1,26 +1,65 @@
 //event listener for button
 document.querySelector('button').addEventListener("click", getInfo);
 
-//Function to fetch info from api
-function getInfo() {
-    var url = "https://www.finnkino.fi/xml/Events/";
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            filterInfo(this);
-        }
-    };
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
+//Variables needed
+var genre;
+
+//After choosing a genre from drop-down menu, filtering information
+function filterMovies() {
+    document.getElementById("moviegenre").select();
+    switch (document.getElementById("moviegenre").value) {
+        case "Animaatio":
+            genre = "Animaatio";
+            break;
+        case "Draama":
+            genre = "Draama";
+            break;
+        default:
+            genre = "";
+            document.getElementById("moviegenre").value = "";
+    }
+    getInfo();
 }
 
-//Function to parse information the way I want it...
+//Function to fetch info from api
+function getInfo() {
+    if (genre != undefined) {
+        var url = "https://www.finnkino.fi/xml/Events/" + genre;
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                filterInfo(this);
+            }
+        }
+        xmlhttp.open("GET", url, true);
+        xmlhttp.send();
+    }
+}
+/* TÄMÄ HETKEKSI PIILOON, EI TOIMINU, KOKEILEN ALEMPAA
+Kokeillaan, että saanko tähän mitään. Genren perusteella?
+function finterInfo(xml) {
+    var x, i, xmlDoc, txt;
+    xmlDoc = xml.responseXML;
+    table1 = "";
+    x = xmlDoc.getElementsByTagName('Event');
+    for (i = 0; i < x.length; i++) {
+        txt += x[i].getAttributeNode('Genres').nodeValue + "<br>";
+    }
+    document.getElementById("data").innerHTML = table1;
+}
+_*/
+
+//Function to parse xml
 function filterInfo(xml) {
     var i;
     var xmlDoc = xml.responseXML;
     var event = xmlDoc.getElementsByTagName("Event");
     var titles = xmlDoc.getElementsByTagName("Title");
     var genres = xmlDoc.getElementsByTagName("Genres");
+    //TARU VOISKO TÄHÄN KOKEILLA ETTÄ JOS EI OLEKAAN JUST SEN GENREN LEFFOJA OLEMASSA, NI ALERT?
+
+   // var image = xmlDoc.getElementsByTagName("EventSmallImagePortrait");
+   // var tableImg = "<img id='movieimg' src='" + xmlDoc.getElementsByTagName("EventSmallImagePortrait")[i].childNodes[0].nodeValue + "'></img>";
     var table1= "<table><tbody><tr><td><strong>Title</strong></td><td><strong>Genre</strong></td></tr>";
     //console.log(xmlDoc);
     for (i = 0; i <event.length; i++) {
@@ -31,9 +70,9 @@ table1 += "</tbody></table>";
 document.getElementById("data").innerHTML = table1;
 }
 
+
+
 /*
-
-
 document.getElementById("info").innerHTML =
 this.responseText;
 //en tiiä mihin väliin tää pitäis tunkee?
@@ -53,3 +92,4 @@ function filterInfo() {
     txt += "</tbody></table>";
     document.getElementById("data").innerHTML = txt;
 }
+*/
